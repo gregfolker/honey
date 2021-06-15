f205ce1463c79f516eebe1952961f45b46a734cf
Greg Folker <gregfolker.dev@gmail.com>
2021-06-15 16:50:43 -0500

    feat: add flags for target directory and verbosity of output logs

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -7,17 +7,31 @@ import (
    "github.com/spf13/cobra"
 )
 
+var (
+   TargetDir string
+   Verbose bool
+)
+
 var rootCmd = &cobra.Command{
-   Use:"honey",
-   Short:"Honey is a utility to convert video files from HBOX format to MP4 format",
+   Use: "honey",
+   Short: "Honey is a utility to convert video files from HBOX format to MP4 format",
    Run: func(cmd *cobra.Command, args []string) {
       // Do stuff here
    },
 }
 
 func Execute() {
+   AddFlags()
+
    if err := rootCmd.Execute(); err != nil {
       fmt.Fprintln(os.Stderr, err)
       os.Exit(1)
    }
+}
+
+func AddFlags() {
+   rootCmd.PersistentFlags().StringVarP(&TargetDir, "target-dir", "t", "", "Path to the directory that contains the *.hbox and *.pll files")
+   rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Enable verbose logging")
+
+   rootCmd.MarkPersistentFlagRequired("target-dir")
 }
